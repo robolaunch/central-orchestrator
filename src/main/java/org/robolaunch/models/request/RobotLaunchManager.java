a877c3efb2a4ab3188a1ff033c3439198a4a0f58
mert18 <mertuygur02@gmail.com>
2022-12-22 11:20:16 +0300

    refactor(robot): :robot: create robot launch manager models.

---
+++ src/main/java/org/robolaunch/models/request/RobotLaunchManager.java
@@ -0,0 +1,37 @@
+package org.robolaunch.models.request;
+
+import java.util.List;
+
+public class RobotLaunchManager {
+   private String targetRobot;
+   private String targetVDI;
+   private List<RobotLaunchManagerLaunchItem> launchItems;
+
+   public RobotLaunchManager() {
+   }
+
+   public String getTargetRobot() {
+      return targetRobot;
+   }
+
+   public void setTargetRobot(String targetRobot) {
+      this.targetRobot = targetRobot;
+   }
+
+   public String getTargetVDI() {
+      return targetVDI;
+   }
+
+   public void setTargetVDI(String targetVDI) {
+      this.targetVDI = targetVDI;
+   }
+
+   public List<RobotLaunchManagerLaunchItem> getLaunchItems() {
+      return launchItems;
+   }
+
+   public void setLaunchItems(List<RobotLaunchManagerLaunchItem> launchItems) {
+      this.launchItems = launchItems;
+   }
+
+}
---
+++ src/main/java/org/robolaunch/models/request/RobotLaunchManagerLaunchItem.java
@@ -0,0 +1,62 @@
+package org.robolaunch.models.request;
+
+public class RobotLaunchManagerLaunchItem {
+   private String name;
+   private String cluster;
+   private String workspace;
+   private String repository;
+   private boolean namespacing;
+   private String launchFilePath;
+
+   public RobotLaunchManagerLaunchItem() {
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getCluster() {
+      return cluster;
+   }
+
+   public void setCluster(String cluster) {
+      this.cluster = cluster;
+   }
+
+   public String getWorkspace() {
+      return workspace;
+   }
+
+   public void setWorkspace(String workspace) {
+      this.workspace = workspace;
+   }
+
+   public String getRepository() {
+      return repository;
+   }
+
+   public void setRepository(String repository) {
+      this.repository = repository;
+   }
+
+   public boolean isNamespacing() {
+      return namespacing;
+   }
+
+   public void setNamespacing(boolean namespacing) {
+      this.namespacing = namespacing;
+   }
+
+   public String getLaunchFilePath() {
+      return launchFilePath;
+   }
+
+   public void setLaunchFilePath(String launchFilePath) {
+      this.launchFilePath = launchFilePath;
+   }
+
+}
