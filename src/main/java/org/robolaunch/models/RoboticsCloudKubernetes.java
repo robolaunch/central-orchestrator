77b858617d0848b87897ee44240e6bfe0366385a
mert18 <mertuygur02@gmail.com>
2023-01-04 00:29:18 +0300

    refactor(kubernetes): :dolphin: add new fields for rc.

--- src/main/java/org/robolaunch/models/RoboticsCloudKubernetes.java
+++ src/main/java/org/robolaunch/models/RoboticsCloudKubernetes.java
@@ -5,6 +5,13 @@ import java.io.Serializable;
 public class RoboticsCloudKubernetes implements Serializable {
    private String processId;
    private String name;
+   private String status;
+   private String instanceType;
+   private String userStage;
+   private String regionName;
+   private String organization;
+   private String team;
+   private String bufferName;
 
    public RoboticsCloudKubernetes() {
    }
@@ -23,6 +30,62 @@ public class RoboticsCloudKubernetes implements Serializable {
 
    public void setName(String name) {
       this.name = name;
+   }
+
+   public String getStatus() {
+      return status;
+   }
+
+   public void setStatus(String status) {
+      this.status = status;
+   }
+
+   public String getInstanceType() {
+      return instanceType;
+   }
+
+   public void setInstanceType(String instanceType) {
+      this.instanceType = instanceType;
+   }
+
+   public String getUserStage() {
+      return userStage;
+   }
+
+   public void setUserStage(String userStage) {
+      this.userStage = userStage;
+   }
+
+   public String getRegionName() {
+      return regionName;
+   }
+
+   public void setRegionName(String regionName) {
+      this.regionName = regionName;
+   }
+
+   public String getOrganization() {
+      return organization;
+   }
+
+   public void setOrganization(String organization) {
+      this.organization = organization;
+   }
+
+   public String getTeam() {
+      return team;
+   }
+
+   public void setTeam(String team) {
+      this.team = team;
+   }
+
+   public String getBufferName() {
+      return bufferName;
+   }
+
+   public void setBufferName(String bufferName) {
+      this.bufferName = bufferName;
    }
 
 }
